07760bb9d23eaa139be8a3c0945c43993d22020e
DESKTOP-1VOJJ7K\nik <nikilip@yandex.ru>
2020-11-19 19:08:21 +0300

    добавил файл Chart1.vue  и подправил ссылки

--- src/components/Chart1.vue
+++ src/components/Chart1.vue
@@ -7,6 +7,20 @@
       </div>
     </div>
   </div>
+  <div class="p-grid p-fluid">
+    <div class="p-col-12 p-lg-6">
+      <div class="card">
+        <h5 class="centerText">Linear Chart2</h5>
+        <Chart type="line" :data="lineData2" />
+      </div>
+    </div>
+  </div>
+
+    <div class="card">
+      <h5>Default</h5>
+      <Button label="Submit" class="p-mr-2 p-mb-2"></Button>
+    </div>
+
 </template>
 
 <script>
@@ -33,7 +47,31 @@ export default {
               borderColor: '#00bb7e'
             }
           ]
+        },
+
+        lineData2: {
+          labels: ['Январь', 'February', 'March', 'April', 'May', 'June', 'July', 'Декабрь'],
+          datasets: [
+            {
+              label: 'Телефония',
+              data: [65, 59, 80, 81, 56, 55, 40, 1000],
+              fill: false,
+              backgroundColor: '#2f4860',
+              borderColor: '#2f4860'
+            },
+            {
+              label: 'Аналоговая телефония',
+              data: [28, 48, 40, 19, 86, 27, 90],
+              fill: false,
+              backgroundColor: '#00bb7e',
+              borderColor: '#00bb7e'
+            }
+          ]
         }
+
+
+
+
       }
     }
 }
